56c8b58915bb59aa20d3e8d557d5dad5cbb077ba
ubuntu <ubuntu@localhost.localdomain>
2020-03-02 15:58:36 +0800

    新增demon06测试代码

--- go/src/demon06/main.go
+++ go/src/demon06/main.go
@@ -3,15 +3,30 @@ package main
 import( "fmt" )
 
 func main(){
+	
 	var a = -65535;
 	b := byte(a)
 	fmt.Println( b )
-	/*
-	val, ok := interface{}(b).(int)
+	type mystring string
+	var str1 mystring = "hello"
+	var str2 string = str1
+	fmt.Println( str2 )
+	val, ok := interface{}(str1).(string)
 	if ok {
-	   fmt.Println( val )
+		fmt.Println( "mystring is equal string" )
+		fmt.Println( val )
 	} else {
-	   fmt.Println( "is not int" )
+		fmt.Println( "mystring is not equil string" )
+		fmt.Println( val )
 	}
-	*/	
+	var s1 []mystring
+	var s2 []string
+	s2 = s1
+	v, ok := interface{}(s2).([]string)
+	fmt.Println( v )
+	if ok {
+		fmt.Println( "[]string == []mystring" )
+	} else {
+		fmt.Println( "[]string != []mystring" )
+	}
 }
